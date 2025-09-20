c53db27839fbf18db650c83eda509de7b04cc134
Jad Ghalayini <jad.ghalayini@protonmail.com>
2025-09-20 22:04:18 +0100

    Beta rules for projections

--- Gt3/JEq/Basic.lean
+++ Gt3/JEq/Basic.lean
@@ -99,6 +99,16 @@ inductive Ctx.JEq : Ctx → Tm 0 → Tm 0 → Tm 0 → Prop
     (rhs_wf : JEq Γ Ba ba ba)
     (hba : JEq Γ Ba (b.lst a) ba)
     : JEq Γ Ba (.app (A.abs b) a) ba
+  | beta_fst' {Γ}  {A : Tm 0} {B : Tm 1} {a b : Tm 0}
+    (hp : JEq Γ (.sigma A B) (.pair a b) (.pair a b))
+    (lhs_wf : JEq Γ A (.fst (.pair a b)) (.fst (.pair a b)))
+    (rhs_wf : JEq Γ A a a)
+    : JEq Γ A (.fst (.pair a b)) a
+  | beta_snd' {Γ}  {A : Tm 0} {B : Tm 1} {a b Ba : Tm 0}
+    (hp : JEq Γ (.sigma A B) (.pair a b) (.pair a b))
+    (lhs_wf : JEq Γ Ba (.snd (.pair a b)) (.snd (.pair a b)))
+    (rhs_wf : JEq Γ Ba b b)
+    : JEq Γ Ba (.snd (.pair a b)) b
   | beta_dite_tt' {Γ} {A : Tm 0} {l r : Tm 1} {lu} {ℓ : ℕ} {L : Finset String}
     (hA : JEq Γ (.univ ℓ) A A)
     (hl : ∀ x ∉ L, JEq (Γ.cons x .unit) A (l.open x) (l.open x))
